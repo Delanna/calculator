b7d328310d2aab6e29eeafea1d81e4d539fd63a1
Delanna <delanna1999@gmail.com>
2023-09-15 18:06:19 -0300

    Finished Product -Calcultor works and looks like a calculator. -could look neater and could have more features but it works just fine. -coding is also very neat

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ let sign;
 const output=document.getElementById("output");
 const buttons=document.getElementById("buttons");
 const operators=document.getElementsByClassName("operator");
+const clearBtn=document.getElementById("clear");
+const equalBtn=document.getElementById("equal");
 
 
 buttons.addEventListener("click",(event)=>{
@@ -23,6 +25,13 @@ buttons.addEventListener("click",(event)=>{
       output.textContent+=button.value;
       num1=output.textContent;
 
+    } else if (button.classList.contains("operator") && num1 && num2 && sign){
+      const operator = event.target;
+      output.textContent=operate(num1,num2,sign);
+      num1=output.textContent;
+      num2=0;
+      sign=operator.value;
+
     } else if (button.classList.contains("operator")){
       const operator = event.target;
       sign=operator.value;
@@ -30,18 +39,28 @@ buttons.addEventListener("click",(event)=>{
   })
 
 
+equalBtn.addEventListener("click",()=>{
+  output.textContent=operate(num1,num2,sign);
+  num1=operate(num1,num2,sign);
+  num2=0;
+  sign="";
+});
+
+clearBtn.addEventListener("click",clear);
+
 
 function operate(num1,num2,sign){
-  if (sign==="+"){
-    add(num1,num2);
-  }else if (sign==="-"){
-    subtract(num1,num2);
-  }else if (sign==="*"){
-    multiply(num1,num2);
-  }else if (sign==="/"){
-    divide(num1,num2);
+  if (sign==="add"){
+    return add(parseInt(num1),parseInt(num2));
+  }else if (sign==="subtract"){
+    return subtract(num1,num2);
+  }else if (sign==="multiply"){
+    return multiply(num1,num2);
+  }else if (sign==="divide"){
+    return divide(num1,num2);
   }
 }
+
 
 function add(a,b){
   return a+b;
@@ -59,3 +78,10 @@ function divide(a,b){
   return a/b;
 }  
 
+
+function clear(){
+  num1=0;
+  num2=0;
+  sign="";
+  output.textContent="";
+}
